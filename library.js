df25776084cf52deb741e17ad3f7bb7fe7d3768f
Dennis Schmalstieg <darianbellentor@outlook.de>
2023-01-20 21:50:31 +0100

    add library functions

--- library.js
+++ library.js
@@ -2,6 +2,16 @@ function $(selector) {
   const self = {
     element: document.querySelector(selector),
     html: () => self.element,
+    on: (event, callback) => {
+      document.addEventListener(event, callback);
+    },
   };
   return self;
 }
+
+console.log($("h1").html());
+
+// create event
+$("h2").on("click", () => {
+  alert("MARIO");
+});
